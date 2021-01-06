00023826c84c721277c6dd539940caa1900f9498
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-06 13:55:30 +0200

    api route fix

--- services/httpService.js
+++ services/httpService.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/api/'
-    : '//localhost:3030/api/'
+    : 'https://todos-next-js.herokuapp.com/api/'
 
 var axios = Axios.create({
     withCredentials: true
