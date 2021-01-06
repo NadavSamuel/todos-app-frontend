3f501e89fceeca48c42e85d48e24f0ac1ad8ad89
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-06 14:03:19 +0200

    third api fix

--- services/httpService.js
+++ services/httpService.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
-    : 'https://todos-next-js.herokuapp.com/api/'
+    ? 'https://todos-next-js.herokuapp.com/api/'
+    : '//localhost:3030/api/'
 
 var axios = Axios.create({
     withCredentials: true
