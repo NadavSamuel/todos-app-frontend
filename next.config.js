0d6437ce1c5015a159b4fa712273a12fcf028542
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2021-01-10 10:55:08 +0200

    next config add 4

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       return [
         {
           // matching all API routes
-          source: "/https://todos-next-js.herokuapp.com/api/:path*",
+          source: "/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
